7d85d00a244c76955af0512c7ed40bcb73cd96e5
Jennifer King <32186812+mairodia@users.noreply.github.com>
2018-09-24 16:59:00 -0700

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,30 @@
+//Programmer: Jennifer King
+//Date: 09/24/2018
+//File: Student.java
+
+public abstract class Student {
+
+	private String name;
+	private String idNumber;
+	private int yearAdmitted;
+	
+	public Student(String n, String id, int year)
+	{
+		name = n;
+		idNumber = id;
+		yearAdmitted = year;
+	}
+	
+	public String toString()
+	{
+			String str;
+			
+			str = "Name: " + name + 
+				  "\nID Number: " + idNumber +
+				  "\nYear Admitted: " + yearAdmitted;
+			
+			return str;
+	}
+
+	public abstract int getRemainingHours();
+}
